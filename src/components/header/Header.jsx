0bf2eb0dc26a67b8695fe87fc2e0b34093601606
Andre TM <andretimoti@yahoo.com>
2023-11-20 21:18:56 +0700

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+// import React from 'react';
+import Styles from './Header.module.css';
+import monkurl from '../../assets/monkey.png';
+import { useState } from "react";
+
+export default function Header() {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <div className={Styles.nav}>
+            <div className={Styles.navbarWrap}>
+                <div className={Styles.navbar}>
+                    <img src={monkurl} alt="monkey" className={Styles.monkImg} />
+                    {/* <div className={Styles.menu} onClick={()=>{alert('clicked!')}}> */}
+                    <div className={Styles.menuWrap}>
+                        <div className={Styles.menu} onClick={() => { setMenuOpen(!isMenuOpen) }}>
+                            {/* <div className={Styles.menu}> */}
+                            <div className={`${Styles.bar1} ${(isMenuOpen) ? Styles.change : ""}`}></div>
+                            <div className={`${Styles.bar2} ${(isMenuOpen) ? Styles.change : ""}`}></div>
+                            <div className={`${Styles.bar3} ${(isMenuOpen) ? Styles.change : ""}`}></div>
+                        </div>
+
+                        <div className={`${Styles.sideBar} ${isMenuOpen ? Styles.menuOpen : Styles.menuClose}`}>
+                            <a href="#">Home</a>
+                            <a href="#">Profile</a>
+                            <a href="#">Capabilities</a>
+                            <a href="#">Contact Me</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
